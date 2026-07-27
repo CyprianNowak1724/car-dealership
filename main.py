674ca33bf771147be4e0d7f3ce1c88dc42eb1197
Cyprian211 <cypriann2009@gmail.com>
2026-07-27 18:17:56 +0200

    Added interactive garage menu

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 from car import Car
 
-list = []
-bmw = Car("BMW", "M3", 2021, 480, 300000)
-mercedes = Car("Mercedes", "C63 AMG", 2020, 510, 350000)
-audi = Car("Audi", "RS5", 2022, 450, 320000)
-list.append(bmw)
-list.append(mercedes)
-list.append(audi)
+garage = []
+is_running = True
 
-for car in list:
-    car.show_info()
-    print("--------------------")
+while is_running:
+    print("=== KOMIS SAMOCHODOWY ===")
+    print("1 - Dodaj samochod")
+    print("2 - Wyswietl samochody")
+    print("3 - Usun samochod")
+    print("4 - Wyjdz")
 
-print(f"Liczba samochodow w garazu: {len(list)}")
+    action = int(input("Wybierz akcje: "))
+
+    match action:
+        case 1:
+            brand = input("Podaj marke: ")
+            model = input("Podaj model: ")
+            year = int(input("Podaj rok produkcji: "))
+            horsepower = int(input("Podaj moc: "))
+            price = float(input("Podaj cene: "))
+
+            new_car = Car(brand, model, year, horsepower, price)
+            garage.append(new_car)
+
+            print("Samochod zostal dodany do garazu.")
+        case 2:
+            if len(garage) == 0:
+                print("Brak samochodow w garazu.")
+            else:
+                for car in garage:
+                    car.show_info()
+        case 3:
+            if len(garage) == 0:
+                print("Brak samochodow w garazu.")
+            else:
+                pass
+        case 4:
+            is_running = False
+            print("Dziekujemy za skorzystanie z naszego komisu samochodowego.")
