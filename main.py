01491c6cc26749b4a1da1ac17b458efbb85fb346
Cyprian211 <cypriann2009@gmail.com>
2026-07-27 18:05:20 +0200

    Added car garage system

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from car import Car
 
+list = []
 bmw = Car("BMW", "M3", 2021, 480, 300000)
-bmw.show_info()
+mercedes = Car("Mercedes", "C63 AMG", 2020, 510, 350000)
+audi = Car("Audi", "RS5", 2022, 450, 320000)
+list.append(bmw)
+list.append(mercedes)
+list.append(audi)
+
+for car in list:
+    car.show_info()
+    print("--------------------")
+
+print(f"Liczba samochodow w garazu: {len(list)}")
